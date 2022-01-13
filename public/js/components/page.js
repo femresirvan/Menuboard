8b465f0b33c561f291905fe076737b1ca3103dfb
femresirvan <femresirvan@gmail.com>
2022-01-13 03:15:26 +0300

    style changes

--- public/js/components/page.js
+++ public/js/components/page.js
@@ -21,7 +21,20 @@ class Page {
     }
 
 }
+{/* <div class="main-wrapper">
+        <h1 class="restaurant-name">PYTHAGORAS RESTAURANT</h1>
+        <div class="content-wrapper1">
+            <div class="text-wrapper">
+                <h1>YEMEK ADI</h1>
+                <p>fiyatı</p>
+                <p>10$</p>
+            </div>
+            <div class="img-wrapper">
+                <img src="https://www.jocooks.com/wp-content/uploads/2016/01/roasted-tomato-soup-1-1.jpg">
+            </div>
+        </div> */}
 
 //DOM VARIABLES
 let oldPage = document.querySelector('#old-page');
 let nextPage = document.querySelector('#next-page');
+let mainFrame = document.querySelector('#main-frame');
